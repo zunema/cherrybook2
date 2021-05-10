3181b4d586cfd4c6a8a9d88bff87a7ee1d3e6ca0
zunema <yxxa.abc@gmail.com>
2021-05-10 07:07:26 +0000

    gate_test.rbの更新

--- cherrybook2/Class/test/gate_test.rb
+++ cherrybook2/Class/test/gate_test.rb
@@ -11,20 +11,15 @@ class GateTest < Minitest::Test
   end
 
   def test_umeda_to_juso
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
   end
 
   def test_umeda_to_mikuni_when_fare_is_not_enough
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
-
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
   end
 
 end
