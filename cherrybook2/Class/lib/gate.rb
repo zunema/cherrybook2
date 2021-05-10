cc5aa853e489dfa4fc7cb8a9d2e0ced7d42a5cde
zunema <yxxa.abc@gmail.com>
2021-05-10 06:40:14 +0000

    gate.rbの更新

--- cherrybook2/Class/lib/gate.rb
+++ cherrybook2/Class/lib/gate.rb
@@ -16,7 +16,7 @@ class Gate
   end
 
   def calc_fare(ticket)
-    from = STSTIONS.index(ticket.stamped_at)
+    from = STATIONS.index(ticket.stamped_at)
     to = STATIONS.index(@name)
     distance = to - from
     FARES[distance - 1]
