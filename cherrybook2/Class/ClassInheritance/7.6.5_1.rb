f8a72a1639bcfd1457f2d35d70639b3043a589b6
zunema <yxxa.abc@gmail.com>
2021-05-18 17:22:57 +0000

    7.6.5_1.rb

---
+++ cherrybook2/Class/ClassInheritance/7.6.5_1.rb
@@ -0,0 +1,30 @@
+class Prodct
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+end
+
+prodct = Prodct.new('A great movie', 1000)
+p prodct.name
+p prodct.price
+
+
+class DVD < Prodct
+  attr_reader :running_time
+
+  def initialize(name, price, running_time)
+    # スーパークラスのinitializeメソッドを呼び出す
+    super(name, price)
+    @running_time = running_time
+  end
+
+end
+
+dvd = DVD.new('A great movie', 1000, 120)
+p dvd.name
+p dvd.price
+p dvd.running_time
